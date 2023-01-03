60bb503c5792e9b815469c842d7beacec6363058
Lee Chih Jung <john.12012000@gmail.com>
2023-01-03 22:20:23 +0000

    Assembled team

--- sample.py
+++ sample.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 EMAIL = os.getenv("EMAIL")
 PASSWORD = os.getenv("PASSWORD")
+TEAM_ID = os.getenv("TEAM_ID")
 
 async def my_team(user_id):
     async with aiohttp.ClientSession() as session:
@@ -14,10 +15,29 @@ async def my_team(user_id):
         await fpl.login(EMAIL, PASSWORD)
         user = await fpl.get_user(user_id)
         team = await user.get_team()
-        player_id = team[0]["element"]
-        player = await fpl.get_player(player_id, include_summary=True)
-        total_points = getattr(player, "total_points", 0.0)
-    print(total_points)
+        gk = list()
+        df = list()
+        md = list()
+        fw = list()
+        for p in team:
+            player_id = p.get("element")
+            player = await fpl.get_player(player_id, include_summary=True)
+            player_firstname = getattr(player, "first_name")
+            player_secondname = getattr(player, "second_name")
+            player_name = f'{player_firstname} {player_secondname}'
+            player_position = getattr(player, "element_type")
+            if player_position == 1:
+                gk.append(player_name)
+            elif player_position == 2:
+                df.append(player_name)
+            elif player_position == 3:
+                md.append(player_name)
+            else:
+                fw.append(player_name)
+    print(gk)
+    print(df)
+    print(md)
+    print(fw)
 
 async def main():
     session = aiohttp.ClientSession()
@@ -28,4 +48,4 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-    asyncio.run(my_team(4905776))
+    asyncio.run(my_team(TEAM_ID))
