8293b3c972d2ea9d723df65aa94fa3f6d3720a2e
Lee Chih Jung <54738966+LordJatonyas@users.noreply.github.com>
2023-01-08 04:58:50 +0000

    Sandbox update, max points algo done

--- src/maximum_points.py
+++ src/maximum_points.py
@@ -1,49 +1,52 @@
 from fpl import FPL
-import aiohttp
 
 class MaximumPoints():
-    def __init__(self, gk, df, md, fw):
-        self.gk = gk
-        self.df = sorted(df)
-        self.md = sorted(md)
-        self.fw = sorted(fw)
+    def __init__(self, players: list[list[tuple]]):
+        self.players = players # each list within is already sorted based on score
 
-    def maxGK(self):
-        return max(self.gk[0], self.gk[-1])
+    def maxGK(self) -> tuple:
+        return self.players[0][-1]
 
-    def maxOutfield(self):
-        bestOutfield = 0
+    def maxOutfield(self) -> list[list[tuple]]:
+        bestOutfield: list[tuple] = [[],[],[]]
+
         # necessary conditions (3 defenders, 2 midfielders, 1 forward)
-        bestOutfield += self.df[-1] + self.df[-2] + self.df[-3]
-        bestOutfield += self.md[-1] + self.md[-2]
-        bestOutfield += self.fw[-1]
+        bestOutfield[2].append(self.players[3][-1])
+        for x in range(3):
+            if x < 2:
+                bestOutfield[1].append(self.players[2][-x-1])
+            bestOutfield[0].append(self.players[1][-x-1])
 
-        remaining = []
-        for i in range(len(self.df) - 3):
-            remaining.append(self.df[i])
-        for j in range(len(self.md) - 2):
-            remaining.append(self.md[j])
-        for k in range(len(self.fw) - 1):
-            remaining.append(self.fw[k])
-        remaining.sort()
+        # Remaining 4 players are picked depending on points
+        # First, fill it with the remaining FW and MD (1 + 3 = 4)
+        bestOutfield[2].append(self.players[3][0])
+        for y in range(3):
+            bestOutfield[1].append(self.players[2][2-y])
+        
+        # Now, compare DF list with what's just added
+        for z in range(2):
+            if bestOutfield[2][-1] < self.players[1][1-z] and len(bestOutfield[2]) > 1:
+                bestOutfield[2].pop()
+                bestOutfield[0].append(self.players[1][1-z])
+                continue
+            if bestOutfield[1][-1] < self.players[1][1-z] and len(bestOutfield[1]) > 2:
+                bestOutfield[1].pop()
+                bestOutfield[0].append(self.players[1][1-z])
+                continue
 
-        for x in range(len(remaining) - 4, len(remaining)):
-            bestOutfield += remaining[x]
         return bestOutfield
 
-    def maxPoints(self):
-        return self.maxGK() + self.maxOutfield()
+    def maxTeam(self) -> list[list[tuple]]:
+        team = self.maxOutfield()
+        team.insert(0, [self.maxGK()])
+        return team
 
-def main():
-    gk = [7,2]
-    df = [2,2,14,5,6]
-    md = [3,3,3,7,10]
-    fw = [9,13,5]
-    maximise = MaximumPoints(gk, df, md, fw)
-    print(maximise.maxGK())
-    print(maximise.maxOutfield())
-    print(maximise.maxPoints())
-    return 0
+    def maxScore(self) -> int:
+        bestScore: int = 0
+        for l in self.maxTeam():
+            for t in l:
+                bestScore += t[1]
+        return bestScore
 
-if __name__ == "__main__":
-    main()
+    def draw_layout(self):
+        return ""
--- src/player_stats.py
+++ src/player_stats.py
@@ -1,7 +1,11 @@
 from fpl import FPL # type: ignore
 import aiohttp
+from operator import itemgetter
 import requests
-from typing import List, Optional
+from typing import Optional
+import maximum_points
+
+import asyncio
 
 class PlayerStats():
     def __init__(self, user_id: Optional[str], email: Optional[str], password: Optional[str]):
@@ -9,44 +13,68 @@ class PlayerStats():
         self.email = email
         self.password = password
     
-    async def get_players(self) -> List[tuple]:
-        # Basic access to FPL API
+    # Output is a list with index 0 -> GK, 1 -> DF, 2 -> MD, 3 -> FW
+    # Each index has a list of tuples (name, score)
+    async def get_players(self) -> list[list[tuple]]:
+        # return variable
+        players: list[list[tuple]] = [[],[],[],[]]
+
+        # Basic access to FPL API, including fail cases
         session = aiohttp.ClientSession()
         fpl = FPL(session)
-        await fpl.login(self.email, self.password)
+        try:
+            await fpl.login(self.email, self.password)
+        except:
+            print("Invalid email or password")
+            await session.close()
+            return players
+        
         user = await fpl.get_user(self.user_id)
-        team = await user.get_team()
+        try:
+            team = await user.get_team()
+        except:
+            print("Invalid user ID")
+            await session.close()
+            return players
+
 
         # variables
         base_url: str = "https://fantasy.premierleague.com/api/"
-        players: List[tuple] = []
-        player_position: str = ""
-        is_starting: bool = True
-        is_captain: bool = False
+        player_id: int = 0
+        player_position: int = 0
+        player_name: str = ""
         player_score: int = 0
-        p_count: int = 0
         
         for p in team:
             player_id = p.get("element")
-            is_captain = p.get("is_captain")
-            p_count += 1
-            if p_count > 11:
-                is_starting = False
             player = await fpl.get_player(player_id, include_summary=True)
-            player_name: str = f'{getattr(player, "first_name")} {getattr(player, "second_name")}'
-            match getattr(player, "element_type"):
-                case 1:
-                    player_position = "GK"
-                case 2:
-                    player_position = "DF"
-                case 3:
-                    player_position = "MD"
-                case 4:
-                    player_position = "FW"
-
             r = requests.get(f"{base_url}element-summary/{player_id}/").json()
-            player_score = r.get("history")[-1].get("total_points")
-            players.append((player_name, player_position, is_starting, is_captain, player_score))
+
+            player_position = getattr(player, "element_type") - 1
+            player_name = f'{getattr(player, "first_name")} {getattr(player, "second_name")}'
+            try:
+                player_score = r.get("history")[-1].get("total_points")
+            except:
+                print("Game hasn't started!")
+                await session.close()
+                return players
+
+            players[player_position].append((player_name, player_score))
 
         await session.close()
+
+        for i in range(len(players)):
+            players[i].sort(key=itemgetter(1))
+
         return players
+
+if __name__ == "__main__":
+    team_id: str = input("Enter your team ID: ")
+    email: str = input("Enter the email address tied to FPL: ")
+    password: str = input("Enter your password: ")
+    ps = PlayerStats(team_id, email, password)
+    players: list[list[tuple]] = asyncio.run(ps.get_players())
+    print(f'{players}\n')
+    object1 = maximum_points.MaximumPoints(players)
+    print(object1.maxTeam())
+    print(object1.maxScore())
